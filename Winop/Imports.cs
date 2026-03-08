a61ca6dc939fe23c1a9439ce72599d1e2fb8fb91
Mika Hyvärinen <mikasamuel45@gmail.com>
2026-03-08 21:57:12 +0200

    Initial commit - rewritten project

--- Winop/Imports.cs
+++
@@ -1,4 +0,0 @@
-global using Winop.Views;
-
-global using Microsoft.UI.Xaml;
-global using Microsoft.UI.Xaml.Controls;
--- scripts/launch-winop.ps1
+++
@@ -1,8 +0,0 @@
-$pkg = Get-AppxPackage -Name '*Winop*' | Select-Object -First 1
-if ($pkg) {
-    Start-Process 'explorer.exe' "shell:AppsFolder\$($pkg.PackageFamilyName)!App"
-    Write-Host 'Launching Winop...' -ForegroundColor Green
-} else {
-    Write-Host 'Winop is not installed. Run build-and-run.bat first, or deploy from Visual Studio (F5).' -ForegroundColor Yellow
-    exit 1
-}
